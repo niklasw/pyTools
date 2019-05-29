b3c02e466eca1a145d17f15f79524a5e8c9a7703
Niklas Wikstrom <niklas.wikstrom@gmail.com>
2019-05-30 00:31:10 +0200

    adding comments...

--- web/flaskRotate/app.py
+++ web/flaskRotate/app.py
@@ -1,12 +1,15 @@
-import os
+import os,glob
 from os.path import join as pjoin
 from flask import Flask,url_for
 from jinja2 import Template
+
+# This is from template.py in this folder
 from template import template
-import glob
 
 app = Flask(__name__)
 
+# Create the actual html that is sent to the client (browser)
+# It just replaces three keywords in the string defined in template.py
 def mkImgPage(imgPaths):
     t = Template(template)
     page = t.render(image     = imgPaths[1], \
@@ -14,12 +17,15 @@ def mkImgPage(imgPaths):
                     prevImage = imgPaths[0])
     return page
 
+# Grab all .jpg images in the static/images/ folder
 def getImageList(imgPath):
     return [os.path.basename(i) for i in glob.glob(pjoin('static','images','*.jpg'))]
 
 def rotate(L, n):
     return L[n:] + L[:n]
 
+
+# All these app.route decorators are Flask stuff.
 @app.route('/')
 def showPage():
     global imageNames
@@ -44,9 +50,8 @@ def fwdPage():
     imageNames = rotate(imageNames,-1)
     return showPage()
 
-
 if __name__ == '__main__':
     imageNames = getImageList(pjoin('static','images'))
 
-    app.run()
+    app.run(host="127.0.0.1", port=5000, debug=True)
 

bb8c27eaaa013fd7606229481f27d331475ead3e
Niklas Wikstrom <niklas.wikstrom@gmail.com>
2019-05-30 17:05:06 +0200

    Added javascript for key press navigation

--- web/flaskRotate/template.py
+++ web/flaskRotate/template.py
@@ -17,8 +17,25 @@ template='''
   <link rel= "stylesheet" type="text/css" href="{{css}}">
 
 </head>
+<script>
+function goLeft()
+{
+    var x = event.which || event.keyCode;
+    if (x == 108)
+    {
+        document.getElementById("left").submit();
+    }
+    if (x == 114)
+    {
+        document.getElementById("right").submit();
+    }
+        
+}
+</script>
+<body onkeypress="goLeft()">
 
 <h1>Rotate with flask</h1>
+<p> Now with javascript navigation: Press "r" or "l"</p>
 <div id="content">
 
     <div id="pane">
@@ -28,16 +45,16 @@ template='''
     </div>
     
     <div id="forms">
-        <form action="/backward/" method="post">
+        <form id="left" action="/backward/" method="post">
             <button  class="lbut" name="previousButton" type="submit">Left</button>
         </form>
         
-        <form action="/forward/" method="post">
+        <form id="right" action="/forward/" method="post">
             <button  class="rbut" name="previousButton" type="submit">Right</button>
         </form>
     </div>
-
 </div>
+</body>
 
 </html>
 '''
